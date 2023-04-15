a2b66be25db26111ca720ede788bafd3d174fbfc
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-04-15 11:28:16 +0200

    Revert "Hw02 express"

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,84 +1,25 @@
-const express = require('express');
-
-const {
-  listContacts,
-  addContact,
-  getContactById,
-  removeContact,
-  updateContact,
-} = require("../../controllers/contacts");
-
-const { contactSchema } = require("../../models/contact");
-const { createError } = require("../../helpers/createError");
+const express = require('express')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  try {
-    const result = await listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  };
+  res.json({ message: 'template message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await getContactById(contactId);
-
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw createError(400, (error.message = 'missing required name field'));
-    }
-    const result = await addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await removeContact(contactId);
-    if (!result) {
-      throw createError(404);
-    }
-    res.json({
-      message: "contact delated",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw createError(400, (error.message = "missing fields"));
-    }
-    const { contactId } = req.params;
-    const result = await updateContact(contactId, req.body);
-    if (!result) {
-      throw createError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+  res.json({ message: 'template message' })
 })
 
-module.exports = router;
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.put('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const app = require('./app')
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server running. Use our API on port: ${port}`);
-});
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000")
+})
