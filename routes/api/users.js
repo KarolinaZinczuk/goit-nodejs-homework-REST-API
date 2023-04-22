47658c5fc5016b87247b3db1c9d7262ae688370f
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-04-22 15:13:21 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,5 @@
 const express = require('express');
 
-
-const path = require("path");
-const fs = require("fs").promises;
-const multer = require("multer");
-const jimp = require("jimp");
-
-
-
 const loginHandler = require("../../auth//loginHandler");
 const auth = require("../../auth/auth");
 
@@ -77,66 +69,5 @@ router.get("/current", auth, async (req, res) => {
         return res.status(500).send({ message: "Server error" });
     }
 });
-
-
-
-
-
-const uploadTmpDir = path.join(process.cwd(), "tmp");
-const avatarsDir = path.join(process.cwd(), "/public/avatars");
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadTmpDir);
-  },
-  avatarFilePath: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  limits: {
-    fileSize: 1048576,
-  },
-});
-
-const upload = multer({
-  storage: storage,
-});
-
-router.patch(
-    "/avatars",
-    auth,
-    upload.single("avatar"),
-    async (req, res, next) => {
-        const { path: temporaryName, originalname: originalName } = req.file;
-
-        const image = await jimp.read(temporaryName);
-        await image.resize(250, 250);
-        await image.writeAsync(temporaryName);
-
-        const { _id } = req.user;
-
-        const userId = req.user.id;
-        const newName = userId + "-" + originalName;
-        const avatarFilePath = path.join(avatarsDir, newName);
-
-        try {
-            await fs.rename(temporaryName, avatarFilePath);
-
-            const newData = { avatarURL: avatarFilePath };
-            await updateUser(_id, newData);
-
-            const user = await getUserById(req.user.id);
-            return res.status(200).json({
-                message: "File uploaded successfully", data: { avatarURL: user.avatarURL },
-            });
-        } catch (error) {
-            console.log(error.message);
-            await fs.unlink(temporaryName);
-            return res.status(401).json({ message: "Not authorized" })
-        }   }
-);
-
-
-
-
 
 module.exports = router;
