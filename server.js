b5625aff4635a8af3e8cba02d480e2b954bf34dd
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-04-22 14:58:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const app = require('./app')
+const fs = require("fs").promises;
+
+const isExist = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false);
+};
+
+const createFolderIfNotExist = async (path) => {
+  // jeeli nasz folder nie istnieje to go utworzymy
+  if (!(await isExist(path))) {
+    await fs.mkdir(path);
+  }
+};
 
 const port = 3000;
 
