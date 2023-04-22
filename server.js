9849b96f3649576a6e49e49696083af6ca5882c8
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-04-22 14:52:18 +0200

    HW04

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const app = require('./app')
+const fs = require("fs").promises;
+
+const isExist = (path) => {
+  return fs
+    .access(path)
+    .then(() => true)
+    .catch(() => false);
+};
+
+const createFolderIfNotExist = async (path) => {
+  // jeeli nasz folder nie istnieje to go utworzymy
+  if (!(await isExist(path))) {
+    await fs.mkdir(path);
+  }
+};
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server running. Use our API on port: ${port}`);
 });
+
+module.exports = createFolderIfNotExist;
