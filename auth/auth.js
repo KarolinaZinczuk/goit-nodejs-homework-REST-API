d3559a9a6aff46ffc83e5d7069993b90654cf246
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-04-23 12:48:43 +0200

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const jwtSecret = process.env.JWT_SECRET;
 
+const { getUserById } = require("../controllers/users");
+
 const auth = async (req, res, next) => {
   const token = req.headers.authorization;
 
