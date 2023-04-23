a3fcdaba0bfe6b941f28ba13e9546fa0a43ee4d4
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-04-23 21:17:37 +0200

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,17 @@
 const { User, hashPassword } = require("../models/user");
 const gravatar = require("gravatar");
+const { v4 } = require("uuid");
 
-const createUser = async (email, password) => {
+const createUser = async (email, password, verificationToken) => {
     const hashedPassword = hashPassword(password);
     const gravatarUrl = gravatar.url(email);
+    const verificationToken = v4();
     try {
         const newUser = new User({
             email,
             password: hashedPassword,
             avatarURL: gravatarUrl,
+            verificationToken,
         });
         await newUser.save();
         return newUser;
