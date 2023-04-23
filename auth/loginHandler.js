e9a194afcc9bc0dd69aa7723b89fe7639ae88e7f
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2023-04-23 16:45:36 +0200

    Update loginHandler.js

--- auth/loginHandler.js
+++ auth/loginHandler.js
@@ -5,17 +5,17 @@ const issueToken = require("./issueToken");
 
 const loginHandler = async (email, incomingPassword) => {
     const user = await getUserByemail(email);
-    if (!user) {
-        return res.status(404).send({ message: "User not found!" });
-    }
-    const userPassword = user.password;
-
-    const result = bcrypt.compareSync(incomingPassword, userPassword);
-    
-    if (result) {
-        return issueToken(user);
-    } else {
-        return res.status(401).send({ message: "Invalid credentials" });
+    try {
+        if (!user) {
+            throw new Error({ message: "User not found!" });
+        }
+        const userPassword = user.password;
+        const result = bcrypt.compareSync(incomingPassword, userPassword);
+        if (result) {
+            return issueToken(user);
+        }
+    } catch (error) {
+        throw new Error({ message: "Invalid credentials" });
     }
 };
 
